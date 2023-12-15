49b64a67e002a52e122d489d568495722409d386
YueXingTong <d00234628@student.dkit.ie>
2023-12-15 18:39:53 +0000

    :sparkles: Implemented function for filtering CIFAR100

--- SmartTechCA1.py
+++ SmartTechCA1.py
@@ -2,11 +2,27 @@ import numpy as np
 from keras.datasets import cifar10, cifar100
 
 # Load CIFAR10 and CIFAR100 data
-(cifar10_x_train, cifar10_y_train), (cifar10_x_train, cifar10_y_train) = cifar10.load_data()
-(cifar100_x_train, cifar100_y_train), (cifar100_x_train, cifar100_y_train) = cifar100.load_data()
+(cifar10_x_train, cifar10_y_train), (cifar10_x_test, cifar10_y_test) = cifar10.load_data()
+(cifar100_x_train, cifar100_y_train), (cifar100_x_test, cifar100_y_test) = cifar100.load_data()
 
 # Select required data from CIFAR10 and CIFAR100
 cifar10_classes = [1, 2, 3, 5, 7, 9]
 cifar100_classes = [2, 8, 11, 13, 19, 34, 35, 41, 46, 47, 48, 52, 56, 58, 59, 65, 80, 89, 90, 96, 98]
 
-def filter_cifar10()
+#Filter CIFAR10 and CIFAR100 data
+def filter_cifar10(cifar10_x_train, cifar10_y_train, cifar10_x_test, cifar10_y_test):
+    cifar10_x_train_filtered = cifar10_x_train[np.isin(cifar10_y_train, cifar10_classes).flatten()]
+    cifar10_y_train_filtered = cifar10_y_train[np.isin(cifar10_y_train, cifar10_classes).flatten()]
+    cifar10_x_test_filtered = cifar10_x_test[np.isin(cifar10_y_test, cifar10_classes).flatten()]
+    cifar10_y_test_filtered = cifar10_y_test[np.isin(cifar10_y_test, cifar10_classes).flatten()]
+    
+    return cifar10_x_train_filtered, cifar10_y_train_filtered, cifar10_x_test_filtered, cifar10_y_test_filtered
+
+def filter_cifar100(cifar100_x_train, cifar100_y_train, cifar100_x_test, cifar100_y_test):
+    cifar100_x_train_filtered = cifar100_x_train[np.isin(cifar100_y_train, cifar100_classes).flatten()]
+    cifar100_y_train_filtered = cifar100_y_train[np.isin(cifar100_y_train, cifar100_classes).flatten()]
+    cifar100_x_test_filtered = cifar100_x_test[np.isin(cifar100_y_test, cifar100_classes).flatten()]
+    cifar100_y_test_filtered = cifar100_y_test[np.isin(cifar100_y_test, cifar100_classes).flatten()]
+    
+    return cifar100_x_train_filtered, cifar100_y_train_filtered, cifar100_x_test_filtered, cifar100_y_test_filtered
+
