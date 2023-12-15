a11728b916cad9346a94154c3a256dae356f0ca7
YueXingTong <d00234628@student.dkit.ie>
2023-12-15 21:49:27 +0000

    :sparkles: Add train the test shape for CIFAR10 and CIFAR100

--- SmartTechCA1.py
+++ SmartTechCA1.py
@@ -31,7 +31,13 @@ def filter_cifar100(cifar100_x_train, cifar100_y_train, cifar100_x_test, cifar10
 cifar10_x_train_filtered, cifar10_y_train_filtered, cifar10_x_test_filtered, cifar10_y_test_filtered = filter_cifar10(cifar10_x_train, cifar10_y_train, cifar10_x_test, cifar10_y_test)
 cifar100_x_train_filtered, cifar100_y_train_filtered, cifar100_x_test_filtered, cifar100_y_test_filtered = filter_cifar100(cifar100_x_train, cifar100_y_train, cifar100_x_test, cifar100_y_test)
 
-# Printing the shape of the filtered CIFAR10 and CIFAR100 data
+# Train and test shape of the CIFAR10 and CIFAR100 data
+print("CIFAR10 X Train Shape: ", cifar10_x_train.shape, ", Y Train Shape: ", cifar10_y_train.shape)
+print("CIFAR10 X Test Shape: ", cifar10_x_test.shape,  ", Y Test Shape: ", cifar10_y_test.shape)
+print("CIFAR100 X Train Shape: ", cifar100_x_train.shape,  ", Y Train Shape: ", cifar100_y_train.shape)
+print("CIFAR100 X Test Shape: ", cifar100_x_test.shape,  ", Y Test Shape: ", cifar100_y_test.shape)
+
+# Train and test shape of the filtered CIFAR10 and CIFAR100 data
 print("Filtered CIFAR10 X Train Shape: ", cifar10_x_train_filtered.shape, ", Y Train Shape: ", cifar10_y_train_filtered.shape)
 print("Filtered CIFAR10 X Test Shape: ", cifar10_x_test_filtered.shape,  ", Y Test Shape: ", cifar10_y_test_filtered.shape)
 print("Filtered CIFAR100 X Train Shape: ", cifar100_x_train_filtered.shape,  ", Y Train Shape: ", cifar100_y_train_filtered.shape)
@@ -50,9 +56,37 @@ def plot_cifar(x_train, y_train, num_of_img):
     plt.show()
 
 # Plot 5 images from CIFAR-10
-print("CIFAR10: ")
 plot_cifar(cifar10_x_train, cifar10_y_train, 5)
 # Plot 5 images from CIFAR-100
-print("\nCIFAR100: ")
 plot_cifar(cifar100_x_train, cifar100_y_train, 5)
 
+# Plotting filted CIFAR10 and CIFAR100 data
+def plot_filtered_cifar(x, y, class_labels, num_images):
+    np.random.seed(0)
+    # Get the number of classes
+    num_classes = len(class_labels)
+    # Create a subplot with a grid of size (num_classes, num_images)
+    fig, axes = plt.subplots(num_classes, num_images, figsize=(2 * num_images, 2 * num_classes))
+    # Adjust the layout for better spacing
+    plt.tight_layout(pad=3.0, h_pad=1.0, w_pad=0.5)
+    
+    # Loop through each class
+    for i, class_label in enumerate(class_labels):
+        # Find indices where the label matches the current class
+        indices = np.where(y == class_label)[0]
+        # Choose random images for each class
+        random_indices = np.random.choice(indices, num_images, replace=False)
+        
+        # Loop through each image in the current class
+        for j, idx in enumerate(random_indices):
+            # Display the image on the subplot
+            axes[i][j].imshow(x[idx])
+            # Turn off the axis for better visualization
+            axes[i][j].axis('off')
+            axes[i][j].set_title(f'Class: {class_label}' if j == 0 else '', size='large')
+    plt.show()
+
+# Plot 5 filtered images from CIFAR-10
+plot_filtered_cifar(cifar10_x_train_filtered, cifar10_y_train_filtered, cifar10_classes, 5)
+# Plot 5 filtered images from CIFAR-100
+plot_filtered_cifar(cifar100_x_train_filtered, cifar100_y_train_filtered, cifar100_classes, 5)
