3e7b8105a359f74c1ceb201feb183317239f0893
YueXingTong <d00234628@student.dkit.ie>
2023-12-17 15:26:25 +0000

    :sparkles: Build leNet model

--- SmartTechCA1.py
+++ SmartTechCA1.py
@@ -159,7 +159,30 @@ def preprocess(img):
 def reshape(images):
     return images.reshape(images.shape[0], 32, 32, 1)
     
-    
+# Build the model
+def leNet_model():
+  model = Sequential()
+  # Convolutional layer with 60 filters, kernel size (5, 5), and ReLU activation
+  model.add(Conv2D(60, (5, 5), input_shape=(32, 32, 1), activation='relu'))
+  # Max pooling layer with pool size (2, 2)
+  model.add(MaxPooling2D(pool_size=(2, 2)))
+  # Convolutional layer with 30 filters, kernel size (3, 3), and ReLU activation
+  model.add(Conv2D(30, (3, 3), activation='relu'))
+  # Max pooling layer with pool size (2, 2)
+  model.add(MaxPooling2D(pool_size=(2, 2)))
+  # Flatten the output for dense layers
+  model.add(Flatten())
+  # Dense layer with 500 neurons and ReLU activation
+  model.add(Dense(500, activation='relu'))
+  # Dropout layer with a dropout rate of 0.5
+  model.add(Dropout(0.5))
+  # Output layer with 'num_classes' neurons and softmax activation
+  model.add(Dense(num_of_class, activation='softmax'))
+  # Compile the model with Adam optimizer, categorical crossentropy loss, and accuracy metric
+  model.compile(Adam(learning_rate=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
+  return model
+
+
 # Display the size of the first image in CIFAR-10
 print("CIFAR-10 Image Size:", cifar10_x_train[0].shape)
 
